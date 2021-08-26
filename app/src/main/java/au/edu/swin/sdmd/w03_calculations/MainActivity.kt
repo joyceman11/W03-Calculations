573c46853e4987150dcdc1c5edd78fdffb8b12fe
joyceman11 <joycemman@gmail.com>
2021-08-26 14:01:15 +1000

    updated the second language option and operators (works better)

--- app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
@@ -2,7 +2,6 @@ package au.edu.swin.sdmd.w03_calculations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -10,50 +9,14 @@ import android.widget.RadioButton
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-var operator ="plus"
-
-//callbacks for lifecycles in the activity
-    override fun onStart() {
-        super.onStart()
-        Log.i("LIFECYCLE", "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i("LIFECYCLE", "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i("LIFECYCLE", "onPause")
-    }
-
-    //stop, restart, destroy
-
-    override fun onStop() {
-        super.onStop()
-        Log.i("LIFECYCLE", "onStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i("LIFECYCLE", "onRestart")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("LIFECYCLE", "onDestroy")
-    }
-
+    var operator = "plus"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("LIFECYCLE", "onCreate")
 
         val number1 = findViewById<EditText>(R.id.number1)
         val number2 = findViewById<EditText>(R.id.number2)
-
-
+        val answer = findViewById<TextView>(R.id.answer)
 
         val equals = findViewById<Button>(R.id.equals)
         equals.setOnClickListener {
@@ -61,38 +24,35 @@ var operator ="plus"
                 "plus" -> add(number1.text.toString(), number2.text.toString())
                 "mult" -> mult(number1.text.toString(), number2.text.toString())
                 else -> add(number1.text.toString(), number2.text.toString())
+            }
 
-            }
-                // TODO: show result on the screen
-                val answer = findViewById<TextView>(R.id.answer)
-                answer.text = result.toString()
-            }
+            // TODO: show result on the screen
+            answer.setText(result.toString())
 
         }
     }
-fun onRadioButtonClicked(view: View) {
-    if (view is RadioButton) {
-        // Is the button now checked?
-        val checked = view.isChecked
 
-        // Check which radio button was clicked
-        when (view.getId()) {
-            R.id.radioPlus ->
-                if (checked) {
-                    var operator = "plus"
-                }
-            R.id.radioMult ->
-                if (checked) {
-                    var operator = "mult"
-                }
+    // adds two numbers together
+    private fun add(number1: String, number2: String) = number1.toInt() + number2.toInt()
+    private fun mult(number1: String, number2: String) = number1.toInt() * number2.toInt()
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            // Is the button now checked?
+            val checked = view.isChecked
+
+            // Check which radio button was clicked
+            when (view.getId()) {
+                R.id.radioPlus ->
+                    if (checked) {
+                        operator ="plus"
+                    }
+                R.id.radioMult ->
+                    if (checked) {
+                        operator = "mult"
+                    }
+            }
         }
     }
+
 }
-
-// adds two numbers together
-private fun add(number1: String, number2: String) = number1.toInt() + number2.toInt()
-private fun mult(number1: String, number2: String) = number1.toInt() * number2.toInt()
-
-
-
-
