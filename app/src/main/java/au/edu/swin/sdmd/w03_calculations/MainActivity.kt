36394f495c0ff903ab9e9a69dfadf165c2fa6bd9
joyceman11 <joycemman@gmail.com>
2021-10-07 14:07:26 +1100

    first commit for week 9 task!

--- app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/w03_calculations/MainActivity.kt
@@ -1,5 +1,6 @@
 package au.edu.swin.sdmd.w03_calculations
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     var operatorResult: Int = 0
@@ -27,6 +29,16 @@ class MainActivity : AppCompatActivity() {
     override fun onPause(){
         super.onPause()
         Log.i("LIFECYCLE", "onPause")
+        val sharedPref = this.getSharedPreferences("testCalcFile",Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()){
+            val number1 = findViewById<TextView>(R.id.number1)
+            putString("num1", number1.text.toString())
+            val number2 = findViewById<TextView>(R.id.number2)
+            putString("num2", number2.text.toString())
+
+            apply()
+        }
+
     }
 
     override fun onStop(){
@@ -47,11 +59,27 @@ class MainActivity : AppCompatActivity() {
         val number2 = findViewById<EditText>(R.id.number2)
         val answer = findViewById<TextView>(R.id.answer)
 
-        //saveInstanceState, used for the result to be kept on the screen
-        savedInstanceState?.let {
-            operatorResult = savedInstanceState.getInt("answer")
-            answer.setText(operatorResult.toString())
+        val sharedPref = this.getSharedPreferences("testCalcFile",Context.MODE_PRIVATE) ?: return
+        val num1 = sharedPref.getString("num1", "0").toString()
+        val num2 = sharedPref.getString("num2", "0").toString()
+        operator = sharedPref.getString("operator", "plus").toString()
+
+        number1.setText(num1)
+        number2.setText(num2)
+
+        operatorResult = when(operator){
+            "plus" -> add(number1.text.toString(), number2.text.toString())
+            "mult" -> mult(number1.text.toString(), number2.text.toString())
+            else -> add(number1.text.toString(), number2.text.toString())
         }
+        answer.setText(operatorResult.toString())
+
+        //saveInstanceState, used for the result to be kept on the screen
+        //savedInstanceState?.let {
+            //operatorResult = savedInstanceState.getInt("answer")
+           // answer.setText(operatorResult.toString())
+        //}
+
         val equals = findViewById<Button>(R.id.equals)
         equals.setOnClickListener {
             val result = when(operator) {
